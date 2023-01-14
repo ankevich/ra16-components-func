69ce38a95ac2537baec5888a3eed073e3e30371c
Annie <overheads165@gmail.com>
2023-01-14 19:13:05 +0100

    app func done

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,20 +1,18 @@
-const ShopItemFunc = (item) => {
+const ShopItemFunc = ({item}) => {
+  const {brand, title, description, descriptionFull, price, currency} = item;
+
   return (
     <div className="main-content">
-      <h2>Tiger of Sweden</h2>
-      <h1>Leonard coat</h1>
-      <h3>Minimalistic coat in cotton-blend</h3>
-      <div className="description">
-        Men's minimalistic overcoat in cotton-blend. Features a stand-up collar,
-        concealed front closure and single back vent. Slim fit with clean,
-        straight shape. Above-knee length.
-      </div>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
       <div className="highlight-window mobile">
         <div className="highlight-overlay"></div>
       </div>
       <div className="divider"></div>
       <div className="purchase-info">
-        <div className="price">£399.00</div>
+        <div className="price">{currency}{price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
