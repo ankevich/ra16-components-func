5fbf967095acbf54b39abfd74218c35f838b262b
Annie <overheads165@gmail.com>
2023-01-14 21:26:46 +0100

    class-based-components

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,22 +1,29 @@
-const ShopItemFunc = ({item}) => {
-  const {brand, title, description, descriptionFull, price, currency} = item;
+import React from 'react';
 
-  return (
-    <div className="main-content">
-      <h2>{brand}</h2>
-      <h1>{title}</h1>
-      <h3>{description}</h3>
-      <div className="description">{descriptionFull}</div>
-      <div className="highlight-window mobile">
-        <div className="highlight-overlay"></div>
+class ShopItemFunc extends React.Component {
+  render() {
+    const {brand, title, description, descriptionFull, price, currency} = this.props.item;
+
+    return (
+      <div className="main-content">
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className="description">{descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">
+            {currency}
+            {price.toFixed(2)}
+          </div>
+          <button>Добавить в корзину</button>
+        </div>
       </div>
-      <div className="divider"></div>
-      <div className="purchase-info">
-        <div className="price">{currency}{price.toFixed(2)}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ShopItemFunc;
